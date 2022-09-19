e7d7f25e91a5898623f156ed100f0a5a3c779603
satopian <satopian@mia.eek.jp>
2022-09-19 15:27:09 +0900

    funftions.phpのバージョン指定を、20220919に。

--- petitnote/index.php
+++ petitnote/index.php
@@ -12,7 +12,7 @@ if(!is_file(__DIR__.'/functions.php')){
 	return die(__DIR__.'/functions.php'.($en ? ' does not exist.':'がありません。'));
 }
 require_once(__DIR__.'/functions.php');
-if(!isset($functions_ver)||$functions_ver<20220918){
+if(!isset($functions_ver)||$functions_ver<20220919){
 	return error($en?'Please update functions.php to the latest version.':'functions.phpを最新版に更新してください。');
 }
 // jQueryバージョン
