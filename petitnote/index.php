9a5c5e700315c601a0cae8c7824b7254a1ed6a63
satopian <satopian@mia.eek.jp>
2022-09-22 13:31:33 +0900

    v0.30.0 Luminous

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.29.5';
-$petit_lot='lot.220921';
+$petit_ver='v0.30.0';
+$petit_lot='lot.220922';
 
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
