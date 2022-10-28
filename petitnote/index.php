8c0c31806baf0c65a2afbbbbed77dbbd5ac5084d
satopian <satopian@mia.eek.jp>
2022-10-28 20:46:01 +0900

    v0.33.6

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,7 +1,7 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.33.5';
+$petit_ver='v0.33.6';
 $petit_lot='lot.221028';
 
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
@@ -12,7 +12,7 @@ if(!is_file(__DIR__.'/functions.php')){
 	return die(__DIR__.'/functions.php'.($en ? ' does not exist.':'がありません。'));
 }
 require_once(__DIR__.'/functions.php');
-if(!isset($functions_ver)||$functions_ver<20221027){
+if(!isset($functions_ver)||$functions_ver<20221028){
 	return die($en?'Please update functions.php to the latest version.':'functions.phpを最新版に更新してください。');
 }
 // jQueryバージョン
