c85a388a36258637bb275ad16249f30dfd58ee73
satopian <satopian@mia.eek.jp>
2023-01-07 22:19:03 +0900

    v0.v0.53.6 xhr→fetch

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.53.5';
-$petit_lot='lot.230106';
+$petit_ver='v0.53.6';
+$petit_lot='lot.230107';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
@@ -913,7 +913,6 @@ function paintcom(){
 	global $usercode; 
 
 	aikotoba_required_to_view();
-	
 	$token=get_csrf_token();
 	$userip = get_uip();
 	//テンポラリ画像リスト作成
