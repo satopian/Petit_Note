02c3f968adc239755c3e9537f85e1cc0d89fab40
satopian <satopian@mia.eek.jp>
2023-01-23 17:32:08 +0900

    v0.56.0 動画非表示オプション、PNG形式のまま投稿するファイルサイズ上限の設定項目の追加など。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.55.3';
-$petit_lot='lot.230122';
+$petit_ver='v0.56.0';
+$petit_lot='lot.230123';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
