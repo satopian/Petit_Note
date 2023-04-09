aef3c0d491c65ddb2c9232d296b08988503cc5fa
satopian <satopian@mia.eek.jp>
2023-04-09 19:52:54 +0900

    v0.63.6 iPadの縦向き横向きでChickenPaintのキャンバスが本来のアスペクト比ではなくなる問題に暫定的に対応。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.63.5';
-$petit_lot='lot.230326';
+$petit_ver='v0.63.6';
+$petit_lot='lot.230409';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
