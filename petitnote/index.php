bae0a44a09f9c96ed4abee8c1dee4c1fb0b7657b
satopian <satopian@mia.eek.jp>
2023-04-21 17:07:45 +0900

    v0.66.2 lot.230422 JavaScriptのキャッシュを読み込まないようにするためクエリに使用しているロット番号を上げた。

--- petitnote/index.php
+++ petitnote/index.php
@@ -2,7 +2,7 @@
 //Petit Note (c)さとぴあ @satopian 2021-2022
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 $petit_ver='v0.66.2';
-$petit_lot='lot.230421';
+$petit_lot='lot.230422';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
