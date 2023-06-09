e64b8c64c1f77e1cb9c5db4882de8c74877a77a2
satopian <satopian@mia.eek.jp>
2023-06-09 22:14:21 +0900

    functions.php　翻訳されたレイヤー名を取得する関数の説明文を追加。

--- petitnote/functions.php
+++ petitnote/functions.php
@@ -1128,7 +1128,7 @@ function check_password_input_error_count(){
 	if(count($arr_err)>=5){
 		error($en?'Rejected.':'拒絶されました。');
 	}
-if(!$admin_pass || !$second_pass || $admin_pass === $second_pass || $admin_pass!==(string)filter_input(INPUT_POST,'adminpass')){
+	if(!$admin_pass || !$second_pass || $admin_pass === $second_pass || $admin_pass!==(string)filter_input(INPUT_POST,'adminpass')){
 		$errlog=$userip."\n";
 		file_put_contents(__DIR__.'/template/errorlog/error.log',$errlog,FILE_APPEND);
 		chmod(__DIR__.'/template/errorlog/error.log',0600);
@@ -1136,32 +1136,34 @@ if(!$admin_pass || !$second_pass || $admin_pass === $second_pass || $admin_pass!
 			safe_unlink(__DIR__.'/template/errorlog/error.log');
 		}
 }
+
+// 優先言語のリストをチェックして対応する言語があればその翻訳されたレイヤー名を返す
 function getTranslatedLayerName() {
-    $acceptedLanguages = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
-    $languageList = explode(',', $acceptedLanguages);
+	$acceptedLanguages = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+	$languageList = explode(',', $acceptedLanguages);
 
-    foreach ($languageList as $language) {
-        $language = strtolower(trim($language));
-        if (strpos($language, 'ja') === 0) {
-            return "レイヤー";
-        }
-        if (strpos($language, 'en') === 0) {
-            return "Layer";
-        }
-        if (strpos($language, 'zh-tw') === 0) {
-            return "圖層";
-        }
-        if (strpos($language, 'zh-cn') === 0) {
-            return "图层";
-        }
-        if (strpos($language, 'fr') === 0) {
-            return "Calque";
-        }
-        if (strpos($language, 'de') === 0) {
-            return "Ebene";
-        }
-    }
+	foreach ($languageList as $language) {
+		$language = strtolower(trim($language));
+		if (strpos($language, 'ja') === 0) {
+			return "レイヤー";
+		}
+		if (strpos($language, 'en') === 0) {
+			return "Layer";
+		}
+		if (strpos($language, 'zh-tw') === 0) {
+			return "圖層";
+		}
+		if (strpos($language, 'zh-cn') === 0) {
+			return "图层";
+		}
+		if (strpos($language, 'fr') === 0) {
+			return "Calque";
+		}
+		if (strpos($language, 'de') === 0) {
+			return "Ebene";
+		}
+	}
 
-    return "Layer";
+	return "Layer";
 }
 
