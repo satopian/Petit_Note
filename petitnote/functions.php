96da518234df78827b9ccd46926424d5422b1a67
satopian <satopian@mia.eek.jp>
2023-07-08 02:36:36 +0900

    v0.80.7

--- petitnote/functions.php
+++ petitnote/functions.php
@@ -1,5 +1,5 @@
 <?php
-$functions_ver=20230706;
+$functions_ver=20230708;
 //編集モードログアウト
 function logout(){
 	$resno=(int)filter_input(INPUT_GET,'resno',FILTER_VALIDATE_INT);
--- petitnote/index.php
+++ petitnote/index.php
@@ -1,7 +1,7 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.80.6';
+$petit_ver='v0.80.7';
 $petit_lot='lot.20230708';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
@@ -16,7 +16,7 @@ if(!is_file(__DIR__.'/functions.php')){
 	return die(__DIR__.'/functions.php'.($en ? ' does not exist.':'がありません。'));
 }
 require_once(__DIR__.'/functions.php');
-if(!isset($functions_ver)||$functions_ver<20230706){
+if(!isset($functions_ver)||$functions_ver<20230708){
 	return die($en?'Please update functions.php to the latest version.':'functions.phpを最新版に更新してください。');
 }
 // jQueryバージョン
