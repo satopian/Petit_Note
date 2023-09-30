61176d69f985ca9fb26d3bb35bc52af970ff7311
satopian <satopian@mia.eek.jp>
2023-09-30 21:28:38 +0900

    v0.92.7

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,7 +1,7 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.92.6';
+$petit_ver='v0.92.7';
 $petit_lot='lot.20230930';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
@@ -26,7 +26,7 @@ if(!isset($misskey_note_ver)||$misskey_note_ver<20230809){
 }
 check_file(__DIR__.'/save.inc.php');
 require_once(__DIR__.'/save.inc.php');
-if(!isset($save_inc_ver)||$save_inc_ver<20230825){
+if(!isset($save_inc_ver)||$save_inc_ver<20230930){
 	return die($en?'Please update save.inc.php to the latest version.':'save.inc.phpを最新版に更新してください。');
 }
 
