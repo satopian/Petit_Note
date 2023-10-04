6933be2ac8db869e9295852c640838c658b4d47b
satopian <satopian@mia.eek.jp>
2023-10-04 20:15:42 +0900

    v0.92.9 Tegakiのサイズ、不透明度の数値入力の改善。type Numberに。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v0.92.8';
-$petit_lot='lot.20231001';
+$petit_ver='v0.92.9';
+$petit_lot='lot.20231003';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
