4187c65af233b2a3ca4d922e0d22fec9e5fc536c
satopian <satopian@mia.eek.jp>
2024-01-10 11:08:59 +0900

    v1.09.9 レイヤーパレットに｢下のレイヤーと結合｣アイコンを追加。グループ結合、全レイヤー結合、マスク適用のショートカットキーを使えるようにした。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.09.8';
-$petit_lot='lot.20240110';
+$petit_ver='v1.09.9';
+$petit_lot='lot.20240111';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
