f1d5aef476e8d89bf487c5daa27fd0a0e0033fad
satopian <satopian@mia.eek.jp>
2024-01-13 09:26:32 +0900

    v1.10.2 シフトキー+左ドラッグで、ブラシサイズと不透明度をより細かく調整できるようにした。それ以外のスライダーはSHIFT押下時も通常の動作のまま。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.10.1';
-$petit_lot='lot.2024011';
+$petit_ver='v1.10.2';
+$petit_lot='lot.2024012';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
