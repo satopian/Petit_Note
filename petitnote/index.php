97a47da7355e62caa371aaf3255339008fff5523
satopian <satopian@mia.eek.jp>
2024-02-12 17:14:17 +0900

    v1.18.3 neo.js 半角スペースありなし。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.18.2';
-$petit_lot='lot.20240211';
+$petit_ver='v1.18.3';
+$petit_lot='lot.20240212';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
