5792c7c8bd70b6f04cf1b3812c755bb5129a2403
satopian <satopian@mia.eek.jp>
2024-05-17 14:53:36 +0900

    v1.32.1 Misskeyへのノートの時は編集ボタン横からの場合でも別タブで開く。アプリ版が起動すると戻ってこれなくなるため。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.32.0';
-$petit_lot='lot.20240516.3';
+$petit_ver='v1.32.1';
+$petit_lot='lot.20240517';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
