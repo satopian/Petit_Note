322e563cd33587e1a8c411846c31486d16b81261
satopian <satopian@mia.eek.jp>
2024-05-23 21:20:09 +0900

    v1.35.1 chickenpaint.js Bootstrap5.3.3

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.35.0';
-$petit_lot='lot.20240521';
+$petit_ver='v1.35.1';
+$petit_lot='lot.20240523';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
