3fcb075b003944fde8876d9b88229eb2b459c355
satopian <satopian@mia.eek.jp>
2024-06-09 23:16:43 +0900

    NEOの下部に表示されているパレットをタップしても、スクリーンキーボードが出現しないように調整。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2023
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.36.27';
-$petit_lot='lot.20240609';
+$petit_ver='v1.37.0';
+$petit_lot='lot.20240610';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
