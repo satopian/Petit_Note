2406225fafb589effb8ed19b2b3b0b3aa03fe034
satopian <satopian@mia.eek.jp>
2024-09-04 13:48:27 +0900

    <style>body{overscroll-behavior-x: none !important; }</style>

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2024
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.50.8';
-$petit_lot='lot.20240903.6';
+$petit_ver='v1.50.9';
+$petit_lot='lot.20240904';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
@@ -2558,7 +2558,7 @@ function res (){
 		if($_res['oya']==='oya'){
 
 			$_res['time_left_to_close_the_thread'] = time_left_to_close_the_thread($_res['time']);
-			$_res['descriptioncom']= $_res['com'] ? h(s(mb_strcut(str_replace('"\n"'," ",$_res['com']),0,300))) : '';
+			$_res['descriptioncom']= $_res['com'] ? h(s(mb_strcut(str_replace("\n","",$_res['com']),0,300))) : '';
 
 			$oyaname = $_res['name'];
 		} 
