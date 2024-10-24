95536255bcc79f4a61baf5b6cc98951250d6fb87
satopian <satopian@mia.eek.jp>
2024-10-24 16:14:20 +0900

    v1.55.2

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2024
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.55.1';
-$petit_lot='lot.20241022';
+$petit_ver='v1.55.2';
+$petit_lot='lot.20241024';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
