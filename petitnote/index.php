ab5573ee6a3b635e0314ef82a2bed7937e2f03bf
satopian <satopian@mia.eek.jp>
2024-12-04 21:48:15 +0900

    v1.60.7 ChickenPaint Beの通信エラー時のエラーメッセージをわかりやすいものに。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2024
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.60.6';
-$petit_lot='lot.20241203';
+$petit_ver='v1.60.7';
+$petit_lot='lot.20241204';
 
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
