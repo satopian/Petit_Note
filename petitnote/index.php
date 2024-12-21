0b4ce139efb7a7e24095b7d6aabf6404a436e7cd
satopian <satopian@mia.eek.jp>
2024-12-21 17:04:40 +0900

    v1.65.0 NEOv1.6.11の"neo_animation_skip"に対応。続きを描く時に動画再生ではなく画像を出す。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2024
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.63.7';
-$petit_lot='lot.20241216';
+$petit_ver='v1.65.0';
+$petit_lot='lot.20241221';
 
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
