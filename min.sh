c66b9fa1411dca1e59375631a42e772870a72e6b
satopian <satopian@mia.eek.jp>
2025-02-27 21:05:46 +0900

    min.sh

---
+++ min.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+google-closure-compiler --js petitnote/template/basic/js/dev/palette.js --js_output_file petitnote/template/basic/js/dev/palette.min.js
+google-closure-compiler --js petitnote/template/basic/js/dev/palette_en.js --js_output_file petitnote/template/basic/js/dev/palette_en.min.js
