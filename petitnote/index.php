036c9435b4bd7251bb6f83913f793a9a089a9ddd
satopian <satopian@mia.eek.jp>
2025-04-02 18:34:27 +0900

    v1.82.6 neo.js chickenpaint.js

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2025
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.82.5';
-$petit_lot='lot.20250331';
+$petit_ver='v1.82.6';
+$petit_lot='lot.20250402';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
