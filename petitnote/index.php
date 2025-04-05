ec2a11e540971896b491c2958d9b45241ade0ea1
satopian <satopian@mia.eek.jp>
2025-04-05 10:19:54 +0900

    v1.82.7 chickenpaint.js

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2025
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.82.6';
-$petit_lot='lot.20250402';
+$petit_ver='v1.82.7';
+$petit_lot='lot.20250405';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
