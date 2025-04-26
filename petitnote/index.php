49eb1649ab67d4390fec4e66e5f98c04ad437176
satopian <satopian@mia.eek.jp>
2025-04-26 16:15:10 +0900

    CSSのテキスト整形。文字フォントをブラウザ標準のゴシック体に。

--- petitnote/index.php
+++ petitnote/index.php
@@ -1,8 +1,8 @@
 <?php
 //Petit Note (c)さとぴあ @satopian 2021-2025
 //1スレッド1ログファイル形式のスレッド式画像掲示板
-$petit_ver='v1.86.0';
-$petit_lot='lot.20250416';
+$petit_ver='v1.86.1';
+$petit_lot='lot.20250426';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
