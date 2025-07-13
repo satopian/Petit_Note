d05c6cb172bffae10311dc5d27154d7cd57cefa4
satopian <satopian@mia.eek.jp>
2025-07-13 16:28:42 +0900

    v1.99.6

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,7 +3,7 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.99.5';
+$petit_ver='v1.99.6';
 $petit_lot='lot.20250713';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
