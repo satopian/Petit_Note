28b1f036dd508c923ded2e2bbdd49e8ad971270d
satopian <satopian@mia.eek.jp>
2025-07-24 10:48:04 +0900

    chickenpaint.jsのバグ修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,7 +3,7 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.100.3';
+$petit_ver='v1.100.5';
 $petit_lot='lot.20250723';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
