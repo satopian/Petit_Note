1e5eee9efbdd809abfbd39efe169fd9a356ef47e
satopian <satopian@mia.eek.jp>
2025-08-16 20:03:02 +0900

    変形時に｢縦横比を維持｣するチェックボックスを表示。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.106.2';
-$petit_lot='lot.20250815';
+$petit_ver='v1.106.3';
+$petit_lot='lot.20250815.1';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
