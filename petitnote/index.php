eb30a8f63b973da69ce8802a7b04d31d803df63f
satopian <satopian@mia.eek.jp>
2025-08-21 22:07:26 +0900

    Enterキーで｢表示をリセット｣した時、ALT+0で｢回転をリセット｣した時にスライダーのバーが同期しないバグを修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.108.0';
-$petit_lot='lot.20250820.5';
+$petit_ver='v1.108.1';
+$petit_lot='lot.20250821';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
