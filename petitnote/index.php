9f6d9b2cd8e17e461e0fe3df6e1ac233102d98a2
satopian <satopian@mia.eek.jp>
2025-09-11 23:02:25 +0900

    テクスチャの拡大率を500%から800%へ。ポップオーバーでサムネイルが隠れないようにした。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.115.1';
-$petit_lot='lot.20250911';
+$petit_ver='v1.115.2';
+$petit_lot='lot.20250912';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
