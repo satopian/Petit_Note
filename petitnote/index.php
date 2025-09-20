53b935df531772bdb2ff05313989744c15b342ee
satopian <satopian@mia.eek.jp>
2025-09-20 18:12:06 +0900

    ChickenPaint Be更新。レイヤーの透明部分に水彩で描くと白く浮き上がる問題を修正。選択範囲内のみ描画。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.120.1';
-$petit_lot='lot.20250918.1';
+$petit_ver='v1.121.2';
+$petit_lot='lot.20250920';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
