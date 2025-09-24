0ff6156d3e072834db039c44f060a170cfdff72b
satopian <satopian@mia.eek.jp>
2025-09-24 18:41:05 +0900

    ChickenPaint Be更新。下のレイヤーと混色。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.121.5';
-$petit_lot='lot.20250922';
+$petit_ver='v1.122.2';
+$petit_lot='lot.20250924';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
