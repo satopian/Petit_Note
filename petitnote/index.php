58c4ea71b98de0735881617d86632fa9fb3fb27f
satopian <satopian@mia.eek.jp>
2025-10-04 18:18:42 +0900

    ChickenPaint Beに色収差フィルタを追加。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.123.1';
-$petit_lot='lot.20250925';
+$petit_ver='v1.125.0';
+$petit_lot='lot.20251004';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
