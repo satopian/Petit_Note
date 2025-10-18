8ea535350ce938274e070325184206838eecea6e
satopian <satopian@mia.eek.jp>
2025-10-18 21:07:43 +0900

    ChickenPaint BeのPCモード/モバイルモード表示をボタンひとつで切り替えられるようにした。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.128.3';
-$petit_lot='lot.20251014';
+$petit_ver='v1.129.3';
+$petit_lot='lot.20251018';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
