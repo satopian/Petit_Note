00c04c1e8fd5e2b274f37682412ef5999be94fe7
satopian <satopian@mia.eek.jp>
2025-11-16 19:08:00 +0900

    Klecks

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.150.0';
-$petit_lot='lot.20251112';
+$petit_ver='v1.151.0';
+$petit_lot='lot.20251116';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
