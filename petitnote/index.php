f2890a88cf6504c0542f6aa050003bde1acd038c
satopian <satopian@mia.eek.jp>
2025-11-28 16:36:05 +0900

    NEO更新。動画再生時にブラウザの高さが足りない時はビューワーの操作バーを上に表示して操作可能にする。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.155.5';
-$petit_lot='lot.20251127';
+$petit_ver='v1.156.0';
+$petit_lot='lot.20251128';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
