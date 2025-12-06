23a57d09689763fb999aaaa0712b314218a576b9
satopian <satopian@mia.eek.jp>
2025-12-06 21:29:47 +0900

    v1.160.0 litaChix更新。縮小表示時のアンチエイリアスの品質向上。全画面モード追加。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.158.5';
-$petit_lot='lot.20251203';
+$petit_ver='v1.160.0';
+$petit_lot='lot.20251206';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
