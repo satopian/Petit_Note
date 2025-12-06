700d129419985d513cb0ebe65e02a76b5b4b0a92
satopian <satopian@mia.eek.jp>
2025-12-07 00:10:08 +0900

    v1.160.1

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.160.0';
-$petit_lot='lot.20251206';
+$petit_ver='v1.160.1';
+$petit_lot='lot.20251207';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
