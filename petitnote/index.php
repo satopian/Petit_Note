5f2585b21cebee2356a0ea5c5367108d9542514f
satopian <satopian@mia.eek.jp>
2025-12-19 20:27:24 +0900

    v1.163.1 litaChix更新。モザイクフィルタを追加。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.162.1';
-$petit_lot='lot.20251214';
+$petit_ver='v1.163.1';
+$petit_lot='lot.20251219';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
