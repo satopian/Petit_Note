3e593961cf4d4a566a6953d5cabad12054bc4734
satopian <satopian@mia.eek.jp>
2026-01-27 14:23:07 +0900

    litaChix更新。レイヤーグループを選択中にグレーアウトしないほうがいいメニュー項目を調整した。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.178.1';
-$petit_lot='lot.20260125';
+$petit_ver='v1.178.2';
+$petit_lot='lot.20260127';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
