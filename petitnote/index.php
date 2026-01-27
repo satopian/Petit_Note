3966d90800efb8c3a8509264ed670dfbee894c48
satopian <satopian@mia.eek.jp>
2026-01-27 20:25:07 +0900

    AXNOS Paint(さとぴあ改造版)がスマホで正常に動作しなくなっていたのを修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.178.2';
-$petit_lot='lot.20260127';
+$petit_ver='v1.178.3';
+$petit_lot='lot.20260127.1';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
