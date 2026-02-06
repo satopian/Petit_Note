41275cd4ae32ebd28788d4dac8b630cfc95f573b
satopian <satopian@mia.eek.jp>
2026-02-06 16:46:16 +0900

    litaChix更新。データ消失防止のためのオートセーブ機能を追加。60ストロークまたは2分間隔でオートセーブ。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.180.0';
-$petit_lot='lot.20260202';
+$petit_ver='v1.181.5';
+$petit_lot='lot.20260206';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
