a13ebcfac00d9fe3dfb8988dc465c3cb60f22cb6
satopian <satopian@mia.eek.jp>
2026-02-16 13:04:22 +0900

    litaChix更新。ウェブワーカーが使えない環境のユーザが存在するため、バケツリレー方式へ。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.186.6';
-$petit_lot='lot.20260215.1';
+$petit_ver='v1.187.0';
+$petit_lot='lot.20260216';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
