bcf07aa2eb9e0efb63f091e9ac7320dd82327cdd
satopian <satopian@mia.eek.jp>
2026-02-22 14:49:56 +0900

    輝度を透明度に変換して描画色に変更。(従来は透明とグレー。今回の更新してカラーピッカーの色に)

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.189.1';
-$petit_lot='lot.20260221';
+$petit_ver='v1.190.0';
+$petit_lot='lot.20260222';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
