58753cbbd499da19749122cc337f84c61360e2bd
satopian <satopian@mia.eek.jp>
2026-02-24 20:40:49 +0900

    動作不良が発生する可能性があるpostできるサーバの設定値に関連する箇所を修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,7 +3,7 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.191.2';
+$petit_ver='v1.191.3';
 $petit_lot='lot.20260224';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
