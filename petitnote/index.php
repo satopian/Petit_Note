a6a0e69b38fb233161612e4ba0f1be507c143aae
satopian <satopian@mia.eek.jp>
2026-03-02 12:02:47 +0900

    Lightbox v2.12.0で画像をコピーできなくなっていたのを修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.193.2';
-$petit_lot='lot.20260301';
+$petit_ver='v1.193.7';
+$petit_lot='lot.20260302';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
