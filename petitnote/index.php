1a66a324310415cc4de86dfaf7578da8079379a5
satopian <satopian@mia.eek.jp>
2026-03-18 23:41:06 +0900

    Tegaki更新。Mobileで動画再生時に画面Touchで移動可能に。表示倍率に70%や150%などを追加。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.197.8';
-$petit_lot='lot.20260318';
+$petit_ver='v1.197.10';
+$petit_lot='lot.20260319';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
