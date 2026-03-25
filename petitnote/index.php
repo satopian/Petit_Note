6c554eeba02ebef222ee80f8605a04ff4be737aa
satopian <satopian@mia.eek.jp>
2026-03-25 13:54:26 +0900

    litaChix更新。混色ブラシツールを透明と混ざるようにした。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v1.198.1';
-$petit_lot='lot.20260323';
+$petit_ver='v1.199.0';
+$petit_lot='lot.20260325';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
