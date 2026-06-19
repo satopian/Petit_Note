c6b5af45d335385353c6e182545e26bcb9885ef8
satopian <satopian@mia.eek.jp>
2026-06-19 23:49:03 +0900

    litaChix更新。混色系ブラシの色の計算にリニアRGBを使用して色の黒ずみを軽減。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v2.0.8';
-$petit_lot='lot.20260617';
+$petit_ver='v2.1.1';
+$petit_lot='lot.20260620';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
