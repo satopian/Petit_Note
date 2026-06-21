7be1c28a5a31dc3edde0d8d681bc09bf6e943864
satopian <satopian@mia.eek.jp>
2026-06-21 20:00:03 +0900

    litaChix更新。Blenderで明るい色と暗い色を混ぜた時に明るい色が優先され明るくなるように。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v2.1.3';
-$petit_lot='lot.20260620.2';
+$petit_ver='v2.1.5';
+$petit_lot='lot.20260621';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
