51b86b23626455755318a8aa0f4feaa8ab08cdce
satopian <satopian@mia.eek.jp>
2026-07-23 12:49:01 +0900

    プロパティに型指定していたため、PHP7.3で動作しなくなっていました。そのため動作環境をPHP7.4以上とします。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,16 +3,16 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v2.8.2';
+$petit_ver='v2.8.3';
 $petit_lot='lot.20260723';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
 
-if (version_compare(PHP_VERSION, '7.3.0', '<')) {
-	die($en? "Error. PHP version 7.3.0 or higher is required for this program to work. <br>\n(Current PHP version:".PHP_VERSION.")":
-		"エラー。本プログラムの動作には PHPバージョン 7.3.0 以上が必要です。<br>\n(現在のPHPバージョン：".PHP_VERSION.")"
+if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+	die($en? "Error. PHP version 7.4.0 or higher is required for this program to work. <br>\n(Current PHP version:".PHP_VERSION.")":
+		"エラー。本プログラムの動作には PHPバージョン 7.4.0 以上が必要です。<br>\n(現在のPHPバージョン：".PHP_VERSION.")"
 	);
 }
 
