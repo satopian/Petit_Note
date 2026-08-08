d7172c14a687ca454ef22069b13a20b6873ca16d
satopian <satopian@mia.eek.jp>
2026-08-08 15:27:52 +0900

    いくつかの配色をもとに戻した。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.0.6';
-$petit_lot='lot.20260807.5';
+$petit_ver='v3.0.7';
+$petit_lot='lot.20260808';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
