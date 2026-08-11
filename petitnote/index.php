ca8769b974ba5403b7371b75f1f35366d59f0d28
satopian <satopian@mia.eek.jp>
2026-08-11 15:04:37 +0900

    いいねボタンで回り込みを解除しないように→以前の画像右の回り込みに近くした。NEOのカラーピッカーの長押しでコンテキストメニューが開いていたのを修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.2.0';
-$petit_lot='lot.20260810';
+$petit_ver='v3.3.0';
+$petit_lot='lot.20260811';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
