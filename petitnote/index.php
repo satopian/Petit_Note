02236ac03e504e7269f66d233c9e6e50690881b6
satopian <satopian@mia.eek.jp>
2026-08-23 01:01:21 +0900

    NEO更新。画像から続きを描いて何も描画せずに投稿を完了すると不正な動画ファイルが作成される問題を修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.10.6';
-$petit_lot='lot.20260822.3';
+$petit_ver='v3.10.7';
+$petit_lot='lot.20260823';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
