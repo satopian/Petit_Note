7c8ce829416e35748e0a4b9f39ddd245fb018872
satopian <satopian@mia.eek.jp>
2026-08-25 16:07:28 +0900

    PHPDoc修正。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.11.0';
-$petit_lot='lot.20260824';
+$petit_ver='v3.11.1';
+$petit_lot='lot.20260825';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
@@ -640,8 +640,8 @@ function post(): void {
 	$pchext = '';
 	//PCHファイルアップロード
 	if ($is_painted_img && $imgfile) {
-		// .pch, .spch,.chi,.psd ブランク どれかが返ってくる
-		if($pchext = check_pch_ext($temp_basepath,['upload'=>true])){
+			// .pch .tgkr .chi .psd "" ブランク どれかが返ってくる
+			if($pchext = check_pch_ext($temp_basepath,['upload'=>true])){
 			$pch_src = $temp_basepath.$pchext;
 			$pch_dst = IMG_DIR.$time.$pchext;
 			if(copy($pch_src, $pch_dst)){
