601e0a360b9ce9d219e28c544cf6663a54bf6404
satopian <satopian@mia.eek.jp>
2026-09-08 22:25:31 +0900

    neo.js

--- petitnote/app/neo/neo.js
+++ petitnote/app/neo/neo.js
@@ -7901,8 +7901,10 @@ Neo.TurnTool = class extends Neo.EffectToolBase {
     this.endX = Math.floor(this.endX);
     this.endY = Math.floor(this.endY);
 
-    this.startX -= 1;
-    this.startY -= 1;
+    if (this.startX >= oe.canvasWidth) this.startX = oe.canvasWidth - 1;
+    if (this.endX >= oe.canvasWidth) this.endX = oe.canvasWidth - 1;
+    if (this.startY >= oe.canvasHeight) this.startY = oe.canvasHeight - 1;
+    if (this.endY >= oe.canvasHeight) this.endY = oe.canvasHeight - 1;
 
     var x = this.startX < this.endX ? this.startX : this.endX;
     var y = this.startY < this.endY ? this.startY : this.endY;
