04e2a877abda0872abfa03e31618a566bd408061
satopian <satopian@mia.eek.jp>
2026-09-26 19:39:50 +0900

    NEO更新。大きなサイズの動画をスマホの横幅にフィットさせる。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.21.6';
-$petit_lot='lot.20260921';
+$petit_ver='v3.22.1';
+$petit_lot='lot.20260925';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
